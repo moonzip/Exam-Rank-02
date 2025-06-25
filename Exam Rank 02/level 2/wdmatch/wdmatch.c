1bd4d55093f2e33bd1ce10549c37fd4fc419a283
moon <158624747+moonzip@users.noreply.github.com>
2025-06-25 18:04:09 +0200

    Add files via upload

---
+++ Exam Rank 02/level 2/wdmatch/wdmatch.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+int     main(int argc, char **argv)
+{
+        int	i;
+        int	j;
+
+        i = 0;
+        j = 0;
+        if (argc == 3)
+        {
+		while (argv[2][j])
+		{
+			if (argv[1][i] == argv[2][j])
+				i++;
+			else
+				j++;
+		}
+		if (argv[1][i] == '\0')
+		{
+			i = 0;
+			while (argv[1][i])
+				write(1, &argv[1][i++], 1);
+		}
+	}
+        write (1, "\n", 1);
+}
+

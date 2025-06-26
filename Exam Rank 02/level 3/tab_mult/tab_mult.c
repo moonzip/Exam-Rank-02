1d96ffc00d2b36e120fe7c78031812eda3bb7e1d
moon <158624747+moonzip@users.noreply.github.com>
2025-06-26 16:37:11 +0200

    Update tab_mult.c

--- Exam Rank 02/level 3/tab_mult/tab_mult.c
+++ Exam Rank 02/level 3/tab_mult/tab_mult.c
@@ -29,9 +29,12 @@ int	ft_atoi(const char *s)
 
 void	ft_putnbr(int nb)
 {
-	if (nb / 10 > 0)
+	if (nb >= 10)
+	{	
 		ft_putnbr (nb / 10);
-	ft_putchar(nb % 10 + '0');
+		ft_putnbr(nb % 10);
+	}
+	ft_putchar(nb + '0');
 }
 
 int	main(int argc, char **argv)

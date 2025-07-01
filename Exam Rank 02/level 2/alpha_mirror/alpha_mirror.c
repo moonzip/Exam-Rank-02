f414128692559f272e93c4945aa249c9dede24ee
moon <158624747+moonzip@users.noreply.github.com>
2025-07-01 09:02:25 +0200

    Update alpha_mirror.c
    Recortado

--- Exam Rank 02/level 2/alpha_mirror/alpha_mirror.c
+++ Exam Rank 02/level 2/alpha_mirror/alpha_mirror.c
@@ -2,26 +2,17 @@
 
 int	main(int argc, char **argv)
 {
-	int	i;
+	int	i = 0;
 
-	i = 0;
 	if (argc == 2)
 	{
-		while (argv[1][i] != '\0')
+		while (argv[1][i])
 		{
 			if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-			{
-				argv[1][i] = 'Z' - (argv[1][i] - 'A');
-				write (1, &argv[1][i], 1);	
-			}
+				argv[1][i] = 'Z' - (argv[1][i] -'A');
 			else if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
-			{
 				argv[1][i] = 'z' - (argv[1][i] - 'a');
-				write (1, &argv[1][i], 1);
-			}
-			else
-				write (1, &argv[1][i], 1);
-			i++;
+			write (1, &argv[1][i++], 1);
 		}
 	}
 	write (1, "\n", 1);

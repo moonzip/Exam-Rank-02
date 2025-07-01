b9aa7614e39867e2c41201dcf106d7233c42bff3
moon <158624747+moonzip@users.noreply.github.com>
2025-07-01 08:56:40 +0200

    Update do_op.c

--- Exam Rank 02/level 2/do_op/do_op.c
+++ Exam Rank 02/level 2/do_op/do_op.c
@@ -15,7 +15,7 @@ int	main(int argc, char **argv)
 		else if (argv[2][0] == '*')
 			printf("%d", atoi(argv[1]) * atoi(argv[3]));
 		else if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' && argv[2][0] != '/')
-			printf("%d", 0);	
+			printf("%d", 0);
 	}
 	printf("\n");
 }

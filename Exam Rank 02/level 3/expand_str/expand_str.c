8e8fcf1e71557c7ea9fe95626e28e597264ef9d3
moon <158624747+moonzip@users.noreply.github.com>
2025-07-09 09:49:11 +0200

    Update expand_str.c

--- Exam Rank 02/level 3/expand_str/expand_str.c
+++ Exam Rank 02/level 3/expand_str/expand_str.c
@@ -20,9 +20,8 @@ int	main(int argc, char **argv)
 				if (spc != '\0')
 					write (1, "   ", 3);
 				spc = 0;
-				write (1, &argv[1][i], 1);
+				write (1, &argv[1][i++], 1);
 			}
-			i++;
 		}
 	}
 	write (1, "\n", 1);
